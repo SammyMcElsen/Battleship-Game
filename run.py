20cd0f38e3b45e902a75073305c1c5b338892d36
Sammy McElsen <samantha.mc.elsen@gmail.com>
2024-02-24 22:42:19 +0000

    added print functions for board

--- run.py
+++ run.py
@@ -6,3 +6,11 @@ COMPUTER_BOARD = [[" "] * 8 for i in range(8)]
 PLAYER_GUESS_BOARD = [[" "] * 8 for i in range(8)]
 COMPUTER_GUESS_BOARD = [[" "] * 8 for i in range(8)]
 LETTERS_TO_NUMBERS = {'A':0, 'B':1, 'C':2, 'D':3,'E':4, 'F':5, 'G':6, 'H':7,}
+
+def print_board(board):
+    print("  A B C D E F G H")
+    print("  +-+-+-+-+-+-+-+")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
