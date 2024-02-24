2e1bca5159aa39ea40ea9ff1ce9129b46d9ab6fe
Sammy McElsen <samantha.mc.elsen@gmail.com>
2024-02-24 22:44:43 +0000

    added function for computer board added check functions to prevent bugs

--- run.py
+++ run.py
@@ -14,3 +14,18 @@ def print_board(board):
     for row in board:
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
+
+def place_ships(board):
+    for ship_length in LENGTH_OF_SHIPS:
+        while True:
+            if board == COMPUTER_BOARD:
+                orientation, row, column = random.choice(["H", "V"]), random.randint(0,7), random.randint(0,7)
+                if check_ship_fit(ship_length, row, column, orientation):
+                    if ship_overlaps(board, row, column, orientation, ship_length) == False:
+                        if orientation == "H":
+                            for i in range(column, column + ship_length):
+                                board[row][i] = "X"
+                        else:
+                            for i in range(row, row + ship_length):
+                                board[i][column] = "X"
+                        break
